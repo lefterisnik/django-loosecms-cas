40d488ccf21cfe6c603ec7a350c1d1d4362f7a37
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-09-30 17:37:43 +0300

    Update url definition according to new core.

--- loosecms_cas/urls.py
+++ loosecms_cas/urls.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url
 
-app_urlpatterns = [
+urlpatterns = [
     url(r'^accounts/login/$', 'django_cas_ng.views.login', name='cas_login'),
     url(r'^accounts/logout/$', 'django_cas_ng.views.logout', name='cas_logout'),
 ]
-
-urlpatterns = []
-
 
